f67ac068883d600b8e3612f441e5bea37c3cc3f4
Takayuki Sase <takayuki.sase@firebear.co.jp>
2025-08-17 14:02:30 +0900

    Google Apps ScriptのURLを最新版（スプレッドシートアクセス修正版）に更新

--- src/app/reservation/page.tsx
+++ src/app/reservation/page.tsx
@@ -78,7 +78,7 @@ export default function ReservationPage() {
     setIsSearching(true);
     
     try {
-      const scriptUrl = 'https://script.google.com/macros/s/AKfycbx6pGfxb8N08UMFL8jIaQVpOlBjYRgb9LLnpGy8EJAoPxigLLoyzqj1p3GW07rUznPysw/exec'; // 予約システム用のURL
+      const scriptUrl = 'https://script.google.com/macros/s/AKfycbwShPf5tx3MIR6joB_kP5_j8wo_RkAY5csiiKSjc7MflpVOlTOoLqsItK_FAu6WJuRWaA/exec'; // 予約システム用のURL
       const callback = 'handleSearchResponse';
       
       const data = {
@@ -141,7 +141,7 @@ export default function ReservationPage() {
     setIsReserving(true);
     
     try {
-      const scriptUrl = 'https://script.google.com/macros/s/AKfycbx6pGfxb8N08UMFL8jIaQVpOlBjYRgb9LLnpGy8EJAoPxigLLoyzqj1p3GW07rUznPysw/exec'; // 予約システム用のURL
+      const scriptUrl = 'https://script.google.com/macros/s/AKfycbwShPf5tx3MIR6joB_kP5_j8wo_RkAY5csiiKSjc7MflpVOlTOoLqsItK_FAu6WJuRWaA/exec'; // 予約システム用のURL
       const callback = 'handleReservationResponse';
       
       const data = {
